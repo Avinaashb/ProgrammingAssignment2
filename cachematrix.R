82fde6215766f7210390f545f77c96cde5f9e9b8
Avinaashb <Avinaash.b@gmail.com>
2014-04-26 15:36:58 +0530

    homework looks to be done.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix-- This function creates a special "matrix" 
+##object that can cache its inverse.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  minv <- NULL
+  set <- function(y) {
+    x <<- y
+    minv <<- NULL
+   }
+  get <- function() x
+  setminv <- function(computedminv) minv <<- computedminv
+  getminv <- function() minv
+  list(set = set, get = get,
+       setminv = setminv,
+       getminv = getminv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve -- This function computes the inverse of the special "matrix"
+## returned by makeCacheMatrix above. If the inverse has already been 
+## calculated (and the matrix has not changed), then the cachesolve 
+## will retrieve the inverse from the cache.
+
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    minv <- x$getminv()
+  ##check to see if the data already exists 
+  ##if so this will return the existing data already stored as minv
+  if(!is.null(minv)) {
+    message("using data from cache")
+    return(minv)
+  }
+  #solves for the data and sets it as minv
+  data <- x$get()
+  minv <- solve(data, ...)
+  x$setminv(minv)
+  minv
 }
